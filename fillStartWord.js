8a0ae78d2681fe4ceca1ffe2664ee85422bfb20d
RewEPIC <78597357+RewEPIC@users.noreply.github.com>
2022-09-08 21:33:33 +0700

    Update fillStartWord.js

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,7 +1,6 @@
 const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
-    return (word === null || word === undefined) ? 
-            undefined : startWord + (word.startsWith(startWord) ? word.slice(startWord.length) : word)
+    return !word ? undefined : startWord + (word.startsWith(startWord) ? word.slice(startWord.length) : word)
 }
 module.exports = fillStartWord
